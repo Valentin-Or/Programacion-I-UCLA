68d53c975c517aea5d3eaee369bd20013956570b
Valentin-Or <1001.31319504.ucla@gmail.com>
2025-03-30 23:13:05 -0400

    herencia2

--- L25.1.P.C2/L25.1.p.c2.Herencia/Ejercicios de practica/Herencia-3/js/Cl_Empleado.js
+++
@@ -1,9 +0,0 @@
-export default class Cl_Empleado {
-  constructor(sueldoBase) {
-    this.sueldoBase = sueldoBase
-  }
-
-  calcularSueldo() {
-    return this.sueldoBase
-  }
-}
--- L25.1.P.C2/L25.1.p.c2.Herencia/Ejercicios de practica/Profesores-A&B/js/Cl_Profesor.js
+++
@@ -1,6 +0,0 @@
-export default class Cl_Profesor {
-  constructor(bono, nombre) {
-    this.bono = bono
-    this.nombre = nombre
-  }
-}
